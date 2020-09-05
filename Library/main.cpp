f26504636ef0180395a9b30aa2017b97665974d1
Aleksandr Larionov <44719816+alej4ndrol@users.noreply.github.com>
2020-09-06 01:59:17 +0900

    Update main.cpp

--- Library/main.cpp
+++ Library/main.cpp
@@ -1,4 +1,4 @@
-﻿//Предметная область : Библиотека
+//Предметная область : Библиотека
 //Информационные объекты : Библиотека, Библиотекарь, Книги, Авторы, Посетитель
 //
 //У книги может быть только один автор.
@@ -13,16 +13,34 @@
 
 using namespace std;
 
+class Book {
+	string book_name;
+public:
+	Book(string book_name) {
+		this->book_name = book_name;
+	};
+
+	string get_book_name() {
+		return book_name;
+	}
+};
+
 class Library {
 	vector<Book> books_arr;
 
-protected:
+public:
+	Library(vector<Book> books_arr) {
+		this->books_arr = books_arr;
+	}
 	void add_book(Book book) {
 		books_arr.push_back(book);
 	}
 
 	void erase_book(int index) {
-		books_arr.erase(books_arr.begin() + index);
+		if (index == -1)
+			cout << "There is no such  book." << endl;
+		else
+			books_arr.erase(books_arr.begin() + index);
 	}
 
 	int find_book(string book_name) {
@@ -35,39 +53,32 @@ protected:
 		}
 		return book_num;
 	}
-public:
-	Library(vector<Book> books_arr){
-		this->books_arr = books_arr;
+
+	void print_book_list() {
+		cout << "Book list: " << endl;
+		for (size_t i = 0; i < books_arr.size(); i++)
+		{
+			cout << i << ". " << books_arr[i].get_book_name() << endl;
+		}
+		cout << endl;
 	}
 };
 
-class Book {
-	string book_name;
-public:
-	Book(string book_name) {
-		this->book_name = book_name;
-	};
-
-	string get_book_name() {
-		return book_name;
-	}
-
-
-};
-
-class Visitor : protected Library {
+class Visitor {
 public:
 
-	void take_book(string book_name) {
-		Library::erase_book(find_book(book_name));
+	void take_book(Library* library, string book_name) {
+		cout << "You took the book: " << "'" << book_name << "'" << endl;
+		library->erase_book(library->find_book(book_name));
+		library->print_book_list();
 	}
 
-	void return_book(string book_name) {
-		Library::add_book(book_name);
+	void return_book(Library* library, string book_name) {
+		library->add_book(book_name);
+		cout << "You returned the book: " << "'" << book_name << "'" << endl;
+		library->print_book_list();
 	}
 };
-
-
 
 int main() {
 
@@ -76,9 +87,12 @@ int main() {
 	Book book2("book_2");
 	Book book3("book_3");
 	Book book4("book_4");
-	Library library({book1, book2, book2, book4 });
-	//library.add_book(book1);
-
+	Library library({ book1, book2, book3, book4 });
+	Alex.take_book(&library, "book_1");
+	Alex.return_book(&library, "book_6");
+	Alex.take_book(&library, "book_7");
+	Alex.take_book(&library, "book_4");
+	Alex.return_book(&library, "book_1");
 
 	return 0;
 }
